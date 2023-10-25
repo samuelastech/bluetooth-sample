b070e431802898f3167792b93f89b26b68cf1ca5
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-10-24 22:50:36 -0300

    feat: useBLE hook

--- App.tsx
+++ App.tsx
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { PermissionsAndroid, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { BleManager, Device } from 'react-native-ble-plx';
+import { useBluetoothLE } from './hooks/useBle';
 
 export default function App() {
+  const { requestPermissions, scanForPeripherals, allDevices, connectToDevice } = useBluetoothLE();
+
+  useEffect(() => {
+    scanForDevices();
+  }, []);
+
+  const scanForDevices = async () => {
+    const isPermissionEnabled = await requestPermissions();
+    if (isPermissionEnabled) {
+      scanForPeripherals();
+    }
+  };
+
+  const t = async (t: any) => {
+    return await t.discoverAllServicesAndCharacteristics()
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text>Stary</Text>
+      </TouchableOpacity>
+      {
+        allDevices.length
+          ? allDevices.map((device: Device) => {
+            console.log(device.id);
+            return(<TouchableOpacity key={device.id} style={styles.device} onPress={() => connectToDevice(device)}>
+              <Text>{device.name}</Text>
+            </TouchableOpacity>)
+          }) : <Text>Nenhum dispositivo...</Text>
+      }
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -17,4 +49,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  button: {
+    backgroundColor: 'yellow',
+    paddingHorizontal: 150,
+    paddingVertical: 50,
+  },
+
+  device: {
+    backgroundColor: 'blue',
+    padding: 50,
+    marginBottom: 5,
+  }
 });
